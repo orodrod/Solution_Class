055675a736b5c89e322d89c7bc2c1792abce7fe7
StevenBS16 <orodriguez1497@gmail.com>
2025-03-17 19:46:10 +0000

    Nuevos arreglos de la documentacion

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Solution solucion = new Solution();
+
+        System.out.println("Ejemplo 1: " + solucion.toCamelCase("omar-Ari-Evelyn-ruth_Alvaro"));
+        System.out.println("Ejemplo 2: " + solucion.toCamelCase("Omar-ari-Evelyn-ruth_Alvaro"));
+        System.out.println("Ejemplo 3: " + solucion.toCamelCase("Omar-ari-Evelyn_Ruth_Alvaro"));
+        System.out.println("Ejemplo 4: " + solucion.toCamelCase("omar-Ari-evelyn_ruth_Alvaro"));
     }
 }
